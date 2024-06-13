c729edf176bca8dbe04776ec0c6737ee8533ad84
yogit4554 <yt7407286@gmail.com>
2024-06-13 22:51:52 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -15,5 +15,10 @@ app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+// routes import 
+import userRouter from "./routes/user.routes.js"
+// routes decleration 
+app.use("/api/v1/users",userRouter);    /// https://localhost:8000/api/v1/users/register
+// since router in different file so we need midleaware to write so app.use will be writen not app.get 
 
 export {app}
