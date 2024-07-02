208cc7af05612e0a918c02f4b4e76c9c0288161b
yogit4554 <yt7407286@gmail.com>
2024-07-02 13:56:06 +0530

    tested Dashboard  Controller

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -9,116 +9,79 @@ import {User} from "../models/user.models.js"
 
 const getChannelStats = asyncHandler(async (req, res) => {
     // Get the channel stats like total video views, total subscribers, total videos, total likes etc.
-    const userId=  req.user._id;
-    if(!isValidObjectId(userId)){
-        throw new ApiError(400,"User id is not correct.")
-    }
-    try {
-        const user =await User.findById(userId);
-        if(!user){
-            throw new ApiError(400,"User not found")
-        }
+    const userId = req.user?._id;
 
-        const totalSubscribers = await Subscription.countDocuments({
-            channel:userId
-        })
-
-        const totalVideos  = await Subscription.countDocuments({
-            owner:userId
-        })
-
-        // total videos views
-        const totalVideosViews = await Video.aggregate(
-            [
-                {
-                    $match: {
-                        owner: new mongoose.Types.ObjectId(userId)
-                    }
-                },
-                {
-                    $match: {
-                        views: {
-                            $gt: 0
-                        }
-                    }
-                },
-                {
-                    $group: {
-                        _id: "$views",
-                        totalViews: {
-                            $sum: "$views"
-                        }
-                    }
-                },
-                {
-                    $project: {
-                        _id: 0,
-                        totalViews: 1
-                    }
+    const totalSubscribers = await Subscription.aggregate([
+        {
+            $match: {
+                channel: new mongoose.Types.ObjectId(userId)
+            }
+        },
+        {
+            $group: {
+                _id: null,
+                subscribersCount: {
+                    $sum: 1
                 }
-            ]
-        )
-
-        const totalVideos_Views = totalVideosViews[0].totalViews;
-
-        if(!totalVideosViews) {
-            throw new ApiError(404, "No videos found while fetching total views");
+            }
         }
+    ]);
 
-        //Total Likes on Videos
-        const totalVideosLikes = await Like.aggregate(
-            [
-                {
-                    $lookup: {
-                        from: "videos",
-                        localField: "video",
-                        foreignField: "_id",
-                        as: "allVideos",
-                    }
+    const video = await Video.aggregate([
+        {
+            $match: {
+                owner: new mongoose.Types.ObjectId(userId)
+            }
+        },
+        {
+            $lookup: {
+                from: "likes",
+                localField: "_id",
+                foreignField: "video",
+                as: "likes"
+            }
+        },
+        {
+            $project: {
+                totalLikes: {
+                    $size: "$likes"
                 },
-                {
-                    $unwind: "$allVideos" //can use addFields->first also 
+                totalViews: "$views",
+                totalVideos: 1
+            }
+        },
+        {
+            $group: {
+                _id: null,
+                totalLikes: {
+                    $sum: "$totalLikes"
                 },
-                {
-                    $match: {
-                        "allVideos.owner": new mongoose.Types.ObjectId(req.user?._id)
-                    }
+                totalViews: {
+                    $sum: "$totalViews"
                 },
-                {
-                    $group: {
-                        _id: null,  //means Single group
-                        totalVideosLikes: {
-                            $sum: 1 //count all the Input Documents in pipeline
-                        }
-                    }
-                },
-                {
-                    $project: {
-                        _id: 0,
-                        totalVideosLikes: 1
-                    }
-                },
-            ]
-        )
-
-        const totalVideos_Likes =  totalVideosLikes[0].totalVideosLikes;
-
-        const channelStats ={
-            totalSubscribers,
-            totalVideos,
-            totalVideos_Views,
-            totalVideos_Likes
+                totalVideos: {
+                    $sum: 1
+                }
+            }
         }
+    ]);
 
-        return res
+    const channelStats = {
+        totalSubscribers: totalSubscribers[0]?.subscribersCount || 0,
+        totalLikes: video[0]?.totalLikes || 0,
+        totalViews: video[0]?.totalViews || 0,
+        totalVideos: video[0]?.totalVideos || 0
+    };
+
+    return res
         .status(200)
         .json(
-        new ApiResponse(200,channelStats,"Channel Stats fetched Successfully.")
-        )
-
-    } catch (error) {
-        throw new ApiError(400,"Error while getting Channel Stats")
-    }
+            new ApiResponse(
+                200,
+                channelStats,
+                "channel stats fetched successfully"
+            )
+        );
 })
 
 const getChannelVideos = asyncHandler(async (req, res) => {
